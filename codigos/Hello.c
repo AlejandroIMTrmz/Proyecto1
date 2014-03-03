53bf841df41cc27d9f6d39ac5b87937d959f48a5
Alejandro Ramírez <alejandro_3091@live.com.mx>
2014-03-03 15:00:30 -0600

    Primer commit de Proyecto1, se establecen los primeros directorios y archivos (readme, .gitignore)

---
+++ codigos/Hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    printf("¡Bienvenido al Proyecto 1!\n");
+    printf("Aprendiendo a hacer poryectos en Git!...\n");
+
+    system("PAUSE");
+    return 0; 
+}
