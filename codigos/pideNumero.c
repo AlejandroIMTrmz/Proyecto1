1bb376ead1ab83221df19e969b345d8818dd4a3b
Alejandro Ramírez <alejandro_3091@live.com.mx>
2014-03-04 14:50:43 -0600

    2do commit: se agrega el programa pideNumero.c

---
+++ codigos/pideNumero.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    
+    printf("Introduce un numero: \n");
+    scanf("%d", &n);
+    printf("El numero es: %d\n", n);
+    
+    system("pause");
+    return 0; 
+}
